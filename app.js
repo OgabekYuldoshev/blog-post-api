d8c037c4072ffd632ea8f04df0cfe7772828662d
OgabekYuldoshev <yuldashoff1@gmail.com>
2022-04-14 16:10:21 +0500

    'cors'

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const swagger = require('./swagger')
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
 const postsRouter = require("./routes/posts");
-
+const cors = require('cors')
 
 const { sequelize } = require("./models");
 
@@ -31,7 +31,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use('/api/swagger', swaggerUI.serve, swaggerUI.setup(swagger))
-
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
